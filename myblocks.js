e62be94006457615160e432e122dbc057ec7184b
Koji Kagawa <kagawa@platy.eng.kagawa-u.ac.jp>
2016-09-04 00:32:37 +0900

    tabs and code generation

--- myblocks.js
+++ myblocks.js
@@ -217,7 +217,7 @@ Blockly.JavaScript['bookcover_line'] = function(block) {
   var value_y1 = Blockly.JavaScript.valueToCode(block, 'y1', Blockly.JavaScript.ORDER_COMMA);
   var value_x2 = Blockly.JavaScript.valueToCode(block, 'x2', Blockly.JavaScript.ORDER_COMMA);
   var value_y2 = Blockly.JavaScript.valueToCode(block, 'y2', Blockly.JavaScript.ORDER_COMMA);
-  var code = 'BC.line(' + value_x1 + ',' +  value_y1 + ',' + 
+  var code = 'BC.line(' + value_x1 + ',' +  value_y1 + ','  
                         + value_x2 + ',' +  value_y2 + ');\n';
   return code;
 };
