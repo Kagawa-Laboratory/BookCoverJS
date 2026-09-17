c0991b7b6f74437becd6f0cfe54f32b5fe1a317d
Koji Kagawa <kagawa.koji@kagawa-u.ac.jp>
2026-09-17 17:59:16 +0900

    Add vite.config.mjs: migrate configuration from vite.config.js to support ES module syntax

---
+++ vite.config.mjs
@@ -0,0 +1,19 @@
+﻿import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: "",  
+  build: {
+    rollupOptions: {
+      input: {
+        main:          resolve(import.meta.dirname, 'index.html'),
+        blockly:       resolve(import.meta.dirname, 'blockly.html'),
+        codemirror:    resolve(import.meta.dirname, 'codemirror.html'),
+        bookCoverTest: resolve(import.meta.dirname, 'bookCoverTest.html'),
+        samples:       resolve(import.meta.dirname, 'samples.html'),
+        tutorial_bookcover: resolve(import.meta.dirname, 'Tutorial/bookcover.html'),
+        tutorial_card:     resolve(import.meta.dirname, 'Tutorial/card.html')
+      },
+    },
+  },
+})
